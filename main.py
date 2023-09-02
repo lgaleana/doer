38f740ae720d96f101b026bbbefdf5f4953dab0f
Luis Galeana <lsgaleana@gmail.com>
2023-09-02 18:27:19 -0400

    Dump logs at end of program or in interrupt

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import signal
+
 from code_tasks import perform_task
 from utils.io import user_input
+from utils import logging
+
+
+signal.signal(signal.SIGINT, logging.interrupt_dump)
+signal.signal(signal.SIGTERM, logging.interrupt_dump)
 
 
 if __name__ == "__main__":
-    task = user_input("Type a google search: ")
-    perform_task(task)
+    try:
+        task = user_input("Type a google search: ")
+        perform_task(task)
+    except:
+        pass
+    finally:
+        logging.dump(name="query")
--- utils/logging.py
+++ utils/logging.py
@@ -8,26 +8,45 @@ from utils.io import print_system
 
 LOG_DIR = "logs"
 
-LOGS = {}
+LOGS = []
 
 
 def log(skip_stdout: bool = False, **kwargs) -> None:
-    LOGS.update(kwargs)
-    if not skip_stdout:
-        for k, w in kwargs.items():
-            print_system(f"{k}: {w}")
+    for k, w in kwargs.items():
+        str_to_log = f"{k}: {w}"
+
+        LOGS.append(str_to_log)
+
+        if not skip_stdout:
+            print_system(str_to_log)
 
 
-def dump() -> None:
+def dump(name: Optional[str] = None) -> None:
+    name_prefix = _find_name_prefix_in_logs(name)
     _check_directory()
 
     now = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
-    log_file = f"{LOG_DIR}/{now}.json"
+    log_file = f"{LOG_DIR}/{name_prefix}{now}.txt"
 
     with open(log_file, "w") as f:
-        f.write(json.dumps(LOGS, indent="\t"))
+        f.write("\n".join(LOGS))
+
+
+def interrupt_dump(signum, frame):
+    dump()
 
 
 def _check_directory() -> None:
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
+
+
+def _find_name_prefix_in_logs(name: Optional[str] = None) -> str:
+    if name:
+        for log in LOGS:
+            chunks = log.split(": ")
+            if chunks[0] == name:
+                name_prefix = "".join(chunks[1:]).replace(" ", "_")
+                name_prefix += "_"
+                return name_prefix
+    return ""
